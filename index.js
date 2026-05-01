8a4a1b1aa396891113a99ff6f216e1f6db02be99
gizemaksoy257 <2024481266@cumhuriyet.edu.tr>
2026-05-01 15:26:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ Sepete Ekle: [Kullanıcıya bu testleri satın alması için çağrıda bulun]
 `;
 
 const model = genAI.getGenerativeModel({ 
-    model: "gemini-1.5-flash",
+    model: "gemini-pro",
     systemInstruction: systemInstruction,
     generationConfig: {
         temperature: 0.3,
